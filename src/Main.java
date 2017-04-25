1cb052d7905d4a16145b97b0a9dcb66c4c1629d8
Scott Pantall <aspantall@gmail.com>
2017-04-25 16:22:09 -0600

    finds correct values but does not seem to compare them correctly

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,24 @@ public class Main {
     
     public static void bitPositions(String line)
     {
+        // Split line into an array of strings
+        String[] tokens = line.split(",");
+        
+        // Turn the first string into an integer
+        //  then turn that into a string of its binary value.
+        String binaryValue = Integer.toBinaryString(Integer.parseInt(tokens[0]));
+        int p1 = Integer.parseInt(tokens[1]);
+        int p2 = Integer.parseInt(tokens[2]);
+        
+        char p1Value = binaryValue.charAt(binaryValue.length() - p1);
+        char p2Value = binaryValue.charAt(binaryValue.length() - p2);
+        
+        System.out.println(tokens[0] + " converted to binary is " + binaryValue);
+        
+        System.out.println("Position " + p1 + ": " + p1Value);
+        System.out.println("Position " + p2 + ": " + p2Value);
+        
+        System.out.println(binaryValue.charAt(binaryValue.length() - p1) == binaryValue.charAt(binaryValue.length() - p1));
         
     }
 }
